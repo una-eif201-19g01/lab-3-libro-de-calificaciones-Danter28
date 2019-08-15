81cdb1e945ef8b7616b90782e9b921855c5f5e32
Danter28 <38162330+Danter28@users.noreply.github.com>
2019-08-15 17:08:15 -0600

    recloned

---
+++ CMakeFiles/LibroCalificaciones.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibroCalificaciones.dir/LibroCalificaciones.cpp.obj"
+  "CMakeFiles/LibroCalificaciones.dir/main.cpp.obj"
+  "LibroCalificaciones.exe"
+  "LibroCalificaciones.exe.manifest"
+  "LibroCalificaciones.pdb"
+  "libLibroCalificaciones.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibroCalificaciones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
